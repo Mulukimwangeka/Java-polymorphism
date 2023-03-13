c55869cc8b38fc6434d441873540647e94043aaf
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-13 10:41:56 +0300

    polymorphism updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+class Animals{
+    public void makeNoise() {
+        System.out.println("generic noise");
+    }
+
+    class pig extends Animals{
+        public void makeNoise() {
+            System.out.println("The pig says :oink");
+        }
+    }
+
+    class dog extends Animals{
+        public void makeNoise() {
+            System.out.println("The dog says :woof");
+        }
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animals pig = new Animals().new pig();
+        Animals dog = new Animals().new dog();
+        pig.makeNoise();
+        dog.makeNoise();
     }
 }
